5b847204de23ae9acce33e5bafee1cb11964db07
Alexabotin <101171432+Alexabotin@users.noreply.github.com>
2022-03-09 17:15:55 +0530

    Update commands.py

--- LuciferMoringstar_Robot/commands.py
+++ LuciferMoringstar_Robot/commands.py
@@ -20,7 +20,7 @@ async def start_message(bot, message):
              InlineKeyboardButton("ℹ️ Help", callback_data="help"),
              InlineKeyboardButton("😎 About", callback_data="about") 
              ],[
-             InlineKeyboardButton("🧐How To Request Movies", url="https://t.me/howtorequestaxm/3"),
+             InlineKeyboardButton("How To Request Movies", url="https://t.me/howtorequestaxm/3"),
              InlineKeyboardButton("🤩 CHANNEL", url="https://t.me/alexamovies_in")
              ]]
         else:
@@ -30,7 +30,7 @@ async def start_message(bot, message):
              InlineKeyboardButton("ℹ️ Help", callback_data="bot_owner"),
              InlineKeyboardButton("😎 About", callback_data="about") 
              ],[
-             InlineKeyboardButton("🧐How To RequestMovies", url="https://t.me/howtorequestaxm/3"),
+             InlineKeyboardButton("How To RequestMovies", url="https://t.me/howtorequestaxm/3"),
              InlineKeyboardButton("🤩 CHANNEL", url="https://t.me/alexamovies_in")
              ]]    
         await message.reply_photo(photo = choice(BOT_PICS), caption=START_MSG.format(mention = message.from_user.mention, bot_name = bot_info.BOT_NAME, bot_username = bot_info.BOT_USERNAME), reply_markup=InlineKeyboardMarkup(buttons))
