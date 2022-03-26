67478e73ea7ef9d8de94e70f58d9de3efe570bed
Alexabotin <101171432+Alexabotin@users.noreply.github.com>
2022-03-26 19:05:20 +0530

    Update callback.py

--- LuciferMoringstar_Robot/callback.py
+++ LuciferMoringstar_Robot/callback.py
@@ -233,7 +233,7 @@ async def cb_handler(client: LuciferMoringstar_Robot, query):
 
         elif query.data.startswith("pmfile"):
             if FORCES_SUB and not await is_subscribed(client, query):
-                await query.answer[Click Here](https://t.me/alexamovies_in),show_alert=True)
+                await query.answer["I Like Your Smartness](But Don't Be Oversmart 😒",show_alert=True)
                 return
             ident, file_id = query.data.split("#")
             filedetails = await get_file_details(file_id)
