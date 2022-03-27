2db725ce9171f30d6a44b697ae8226f5d848b6cc
Alexabotin <101171432+Alexabotin@users.noreply.github.com>
2022-03-27 13:34:22 +0530

    Update callback.py

--- LuciferMoringstar_Robot/callback.py
+++ LuciferMoringstar_Robot/callback.py
@@ -258,8 +258,7 @@ async def cb_handler(client: LuciferMoringstar_Robot, query):
                         chat_id=query.from_user.id,
                         file_id=file_id,
                         caption=f_caption
-                        )U
-            
+                        )
                 await query.answer(url=f"https://t.me/{bot_info.BOT_USERNAME}?start=subscribe")
             except Exception as e:
                 await query.answer(url=f"https://t.me/{bot_info.BOT_USERNAME}?start=subscribe")
